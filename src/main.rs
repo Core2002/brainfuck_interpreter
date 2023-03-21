fbc07632914451a00452da10b2737e3e2a30245f
NekokeCore <Core2002@163.com>
2023-03-21 17:53:41 +0800

    实现了HelloWorld

--- src/main.rs
+++ src/main.rs
@@ -8,36 +8,51 @@
 // [ 	If the byte at the data pointer is zero, then instead of moving the instruction pointer forward to the next command, jump it forward to the command after the matching ] command.
 // ] 	If the byte at the data pointer is nonzero, then instead of moving the instruction pointer forward to the next command, jump it back to the command after the matching [ command.
 
-fn main() {
-    let mut bf_p: usize = 0;
-    let mut bf_v: Vec<i32> = vec![0; 32];
+use std::io::{self, Write};
 
-    // let bf_i = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.".chars();
-    let bf_i = "+++++>>+++>>>,..>+++..]".chars();
-    for c in bf_i {
-        match c {
-            '>' => bf_p += 1,
-            '<' => bf_p -= 1,
-            '+' => bf_v[bf_p] = bf_v[bf_p] + 1,
-            '-' => bf_v[bf_p] = bf_v[bf_p] - 1,
+fn main() {
+    let bf_instructions: Vec<char> = "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.>+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.>++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.>++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.>+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.>++++++++++++++++++++++++++++++++.>+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.>+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.>++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.>++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.>++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.>+++++++++++++++++++++++++++++++++.>".chars().collect();
+    let mut bf_instruction_ptr: usize = 0;
+
+    let mut bf_memory: Vec<i32> = vec![0; 32];
+    let mut bf_memory_ptr: usize = 0;
+
+    let mut last_bracket_pos: Vec<usize> = Vec::new();
+    while bf_instruction_ptr < bf_instructions.len() {
+        match bf_instructions[bf_instruction_ptr] {
+            '>' => bf_memory_ptr += 1,
+            '<' => bf_memory_ptr -= 1,
+            '+' => bf_memory[bf_memory_ptr] = bf_memory[bf_memory_ptr] + 1,
+            '-' => bf_memory[bf_memory_ptr] = bf_memory[bf_memory_ptr] - 1,
             '.' => {
-                let p_c = std::char::from_u32(bf_v[bf_p] as u32).unwrap();
-                print!("{}", p_c)
+                let printed_char = std::char::from_u32(bf_memory[bf_memory_ptr] as u32).unwrap();
+                print!("{}", printed_char)
             }
             ',' => {
-                println!("\ninput char>");
+                print!("input char>");
+                io::stdout().flush().unwrap();
                 let mut buf = String::new();
                 std::io::stdin().read_line(&mut buf).unwrap();
-                bf_v[bf_p] = (buf.chars().next().unwrap() as u32) as i32;
+                bf_memory[bf_memory_ptr] = (buf.chars().next().unwrap() as u32) as i32;
+            }
+            '[' => {
+                if bf_memory[bf_memory_ptr] != 0 {
+                    last_bracket_pos.push(bf_instruction_ptr);
+                }
+            }
+            ']' => {
+                if bf_memory[bf_memory_ptr] == 0 {
+                    bf_instruction_ptr = last_bracket_pos.pop().unwrap();
+                }
             }
             _ => {
-                print!("\n<{}> |", bf_p);
-                for x in &bf_v {
+                print!("\n<{}> |", bf_memory_ptr);
+                for x in &bf_memory {
                     print!(" {} |", x)
                 }
                 println!();
             }
         }
+        bf_instruction_ptr += 1;
     }
-
 }
